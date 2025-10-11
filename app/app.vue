3c7a79b5d5b7b83e434e22d2b916ffdb465b7b57
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2025-10-11 05:05:00 +0100

    feat: Install vercel speed insights

--- app/app.vue
+++ app/app.vue
@@ -4,9 +4,11 @@
       <NuxtPage />
     </NuxtLayout>
     <Analytics />
+    <SpeedInsights />
   </UApp>
 </template>
 
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
