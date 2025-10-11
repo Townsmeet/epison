bb300f389198b6598d78e2e29eaeba9877d47d58
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2025-10-11 04:55:31 +0100

    feat: Install vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,10 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <Analytics />
   </UApp>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
